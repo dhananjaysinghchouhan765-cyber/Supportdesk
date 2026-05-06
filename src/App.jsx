002df5059683cf4d0849c96fd71f6542a301d391
dhananjaysinghchouhan765-cyber <dhananjaysinghchouhan765@gmail.com>
2026-05-07 00:25:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-                import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
